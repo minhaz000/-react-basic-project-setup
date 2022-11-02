de8e331c71b8ab5c93586dc0fa3c6453ba3445c9
minhaz000 <mrmminhaz@gamail.com>
2022-11-02 22:48:22 +0600

    basic setup for a react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+import { RootContext } from './context/RootContext';
+import { createBrowserRouter, RouterProvider, Route, Link, } from "react-router-dom";
+import Layout from './layout/Layout'
+import Home from './components/Home/Home'
+function App(props) {
+      const {user} = useContext(RootContext)
 
-function App() {
+  const router = createBrowserRouter([
+            {
+              path: "/",
+              element: <Layout> </Layout>,
+              children:[
+                {
+                  path:"",
+                  element:<Home> </Home>
+                }
+              ]
+            } 
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       
+
+    <RouterProvider router={router} />     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import Context from './context/RootContext';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Context>
+        <App />
+    </Context>
+   
   </React.StrictMode>
 );
 
