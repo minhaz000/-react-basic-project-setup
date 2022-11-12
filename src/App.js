710a015c38e451f99a232ffba2896c30464022a8
minhaz000 <mrmminhaz@gamail.com>
2022-11-12 11:20:51 +0600

    saparate ruter page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import React, { useContext } from 'react';
 import { RootContext } from './context/RootContext';
-import { createBrowserRouter, RouterProvider, Route, Link, } from "react-router-dom";
-import Layout from './layout/Layout'
-import Home from './components/Home/Home'
+import {RouterProvider} from "react-router-dom";
+import router from './Routes/Routes'
 function App(props) {
       const {user} = useContext(RootContext)
 
-  const router = createBrowserRouter([
-            {
-              path: "/",
-              element: <Layout> </Layout>,
-              children:[
-                {
-                  path:"",
-                  element:<Home> </Home>
-                }
-              ]
-            } 
-  ])
+  
 
   return (
     <div>
